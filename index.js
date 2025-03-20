02406b8b1dc0fdc1744d0a849b1fe8c2a1fdd289
Ryan mugei <Lquid1264@gmail.com>
2025-03-20 12:39:37 +0300

    map method lab

--- index.js
+++ index.js
@@ -12,5 +12,10 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((element) => {
+    return element
+      .split(" ")
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(" ");
+  });
 }
